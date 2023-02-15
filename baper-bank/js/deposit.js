bc958aeee13c5ace96ebabcfbaa0912786a55ef8
jihad-hossain1 <jihadkhan934@gmail.com>
2023-02-15 09:04:51 +0600

    deposit section complete

--- baper-bank/js/deposit.js
+++ baper-bank/js/deposit.js
@@ -4,14 +4,30 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // step-2 : get the deposit amount from the deposit input field
     // for input field use .value to the value inside the input field
     const depositField = document.getElementById('deposit-field');
-    const newDepositAmount = depositField.value ;
+    const newDepositAmountString = depositField.value ;
+    const newDepositAmount = parseFloat(newDepositAmountString);
     
     // step-3 : get the current deposit total
     // for non-input(element other than input, textarea) usr innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const previousDepositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    depositTotalElement.innerText = newDepositAmount;
+
+    // step-4 : add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
 
     // clear the deposit field
     depositField.value = '';
+
+    // step-5 : get balance current total 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-6 : calculate current balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
 })
